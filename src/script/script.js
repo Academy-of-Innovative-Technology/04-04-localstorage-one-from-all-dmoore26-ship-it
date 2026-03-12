fb5d992c4ffbc96635785bb8f1c2e0a733356d5e
Dylin Moore <dmoore26@aoiths.org>
2026-03-12 14:00:53 -0400

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,61 +1,93 @@
 var Database = {
-  keyName: "Mutant Database",
-  data: []
+ keyName: "Mutant Database",
+ data: []
 };
 
 function loadDataSource() {
+ let rawData = localStorage.getItem(Database.keyName);
+ let parsedData = JSON.parse(rawData);
+ Database.data = parsedData.response;
+ let select = document.querySelector("#select-mutant");
+ Database.data.forEach(function(mutant) {
+   var let = `<option value="${mutant.name.alias}">${mutant.name.alias}</option>`;
 
+   select.insertAdjacentHTML("beforeend", option);
+
+ });
 
 }
 
 
 function searchForMutantByAlias(mutantAlias) {
 
-  var index = -1;
+ var index = -1;
 
+ Database.data.forEach(function(mutant, i){
 
+   if(mutant.name.alias === mutantAlias){
+     index = i;
+   }
 
+ });
 
-
-
-  if (index === -1) {
-    alert("Invalid Alias");
-  } else {
-    displayData(index);
-  }
+ if (index === -1) {
+   alert("Invalid Alias");
+ } else {
+   displayData(index);
+ }
 }
 
 
 function displayData(index) {
 
-  var htmlTemplate = ``;
+ let mutant = Database.data[index];
 
+ let powers = mutant.powers.join(", ");
+ let affiliations = mutant.affiliation.join(", ");
 
+ let htmlTemplate = `
 
+ <div class="card">
+   <div class="row g-0">
 
+     <div class="col-md-4">
+       <img src="${mutant.image}" class="img-fluid rounded-start">
+     </div>
 
+     <div class="col-md-8">
+       <div class="card-body">
 
+         <h3>${mutant.name.alias}</h3>
+         <p><strong>Name:</strong> ${mutant.name.firstName} ${mutant.name.lastName}</p>
 
+         <p><strong>Gender:</strong> ${mutant.profile.gender}</p>
+         <p><strong>Eyes:</strong> ${mutant.profile.eyes}</p>
+         <p><strong>Hair:</strong> ${mutant.profile.hair}</p>
+         <p><strong>Height:</strong> ${mutant.profile.height}</p>
 
-  document.querySelector("#results-section").innerHTML = htmlTemplate;
+         <p><strong>Powers:</strong> ${powers}</p>
+         <p><strong>Affiliations:</strong> ${affiliations}</p>
+
+       </div>
+     </div>
+
+   </div>
+ </div>
+
+ `;
+
+ document.querySelector("#results-section").innerHTML = htmlTemplate;
 }
 
 
 function getSelectedValue() {
-  const selectElement = document.querySelector('#select-mutant');
-  const selectedOption = selectElement.options[selectElement.selectedIndex];
-  const selectedText = selectedOption.text;
+let selectElement = document.querySelector("#select-mutant");
+let selectedOption = selectElement.options[selectElement.selectedIndex];
+ let selectedText = selectedOption.text;
 
-  // TEST METHOD // REMOVE OR HIDE
-  alert(selectedText);
-
-  searchForMutantByAlias(selectedText);
-
+ searchForMutantByAlias(selectedText);
 }
 
 document.querySelector("#select-mutant").addEventListener('change', getSelectedValue);
 
-
-
-// CALL BY DEFAULT
 loadDataSource();
